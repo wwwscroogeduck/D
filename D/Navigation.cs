95478da1397dbc17c1d1dc4262ac864f9e6c37d4
wwwscroogeduck <polina.plotnicova2017@mail.ru>
2025-06-09 16:53:35 +0300

    commit1

---
+++ D/Navigation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace D
+{
+    public class Navigation
+    {
+
+        //Навигация между страницами
+     public static Frame CurrentFrame {  get; set; }
+        public static Window Root {  get; set; }
+    }
+}
---
+++ D/Pages/PartnerPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace D.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PartnerPage.xaml
+    /// </summary>
+    public partial class PartnerPage : Page
+    {
+        public PartnerPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Partner.ItemsSource = Database.Instance.Partner.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Navigation.CurrentFrame.Navigate(new Users());
+        }
+    }
+}
